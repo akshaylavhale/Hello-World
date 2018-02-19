8a91a618fb8c05cf98d98288f65358d70e2eb8ff
akshaylavhale <akshaylavhale14@gmail.com>
2018-02-19 02:22:54 -0500

    Test Project

---
+++ src/main/java/programs/Test.java
@@ -0,0 +1,10 @@
+package programs;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my Test program");
+
+	}
+
+}
